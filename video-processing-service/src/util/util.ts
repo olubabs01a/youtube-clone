4c9a267a0dc958f18ce0240e371c5b67dbc785b6
Funmi B <osb1271.rit@gmail.com>
2024-01-03 02:19:57 -0500

    refac util

--- video-processing-service/src/util/util.ts
+++ video-processing-service/src/util/util.ts
@@ -1,7 +1,35 @@
-export function isNullOrUndefined(obj: any) {
-  return obj === null || obj === undefined;
+/**
+ * @param obj - Object to validate as non-null.
+ * @returns {boolean}
+ */
+export function isNullOrUndefined(obj: any): obj is null | undefined {
+  return obj === null || obj === undefined || typeof obj === "undefined";
 }
 
-export function isNullOrEmptyString(obj: string) {
-  return isNullOrUndefined(obj) || obj.trim().length === 0;
+/**
+ * @param obj - Object to validate as non-null.
+ * @returns {boolean}
+ */
+export function isNullOrEmptyObject(obj: any) {
+  if (isNullOrUndefined(obj)) {
+    return true;
+  } else if (typeof obj !== "object") {
+    throw new Error(`Invalid type provided for object: '${obj}'`);
+  }
+
+  return obj.toString() !== "{}";
+}
+
+/**
+ * @param obj - Object to validate as non-empty string.
+ * @returns {boolean}
+ */
+export function isNullOrEmptyString(obj: any) {
+  if (isNullOrUndefined(obj)) {
+    return true;
+  } else if (typeof obj !== "string") {
+    throw new Error(`Invalid type provided for string: '${obj}'`);
+  }
+
+  return obj.trim().length === 0;
 }
