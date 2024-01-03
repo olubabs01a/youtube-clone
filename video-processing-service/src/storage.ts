1600d36a94470cf2fc79a3881f885cc1802c5370
Funmi B <osb1271.rit@gmail.com>
2024-01-02 23:30:32 -0500

    add video convert method

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -4,3 +4,32 @@ import ffmpeg from "fluent-ffmpeg";
 
 // Creates a client
 const storage = new Storage();
+
+/**
+ * Creates local directories for raw and processed videos
+ */
+export function setupDirectories() {
+
+}
+
+/**
+ * Convert provided raw video and save as provided processed file name.
+ * @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}
+ * @param processedVideoName - The name of the file to convert to {@link localProcessedVideoPath}
+ * @returns A promise that resolves when the video has been converted.
+ */
+export function convertVideo(rawVideoName: string, processedVideoName: string) {
+    return new Promise<void>((resolve, reject) => {
+        ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
+            .outputOptions("-vf", "scale=-1:360") // 360p
+            .on("end", function () {
+                console.info("Processing finished successfully");
+                resolve();
+            })
+            .on("error", (err) => {
+                console.error(`An error occurred: ${err.message}`);
+                reject(err);
+            })
+            .save(`${processedVideoPath}/${processedVideoName}`);
+    });
+}
