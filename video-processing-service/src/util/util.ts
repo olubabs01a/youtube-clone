17f1fd5f59f4f453c38d7f39bffac4dd30be3c9b
Funmi B <osb1271.rit@gmail.com>
2024-01-03 00:30:14 -0500

    update string util

--- video-processing-service/src/util/util.ts
+++ video-processing-service/src/util/util.ts
@@ -2,6 +2,6 @@ export function isNullOrUndefined(obj: any) {
   return obj === null || obj === undefined;
 }
 
-export function isNullOrUndefinedString(obj: string) {
+export function isNullOrEmptyString(obj: string) {
   return isNullOrUndefined(obj) || obj.trim().length === 0;
 }
