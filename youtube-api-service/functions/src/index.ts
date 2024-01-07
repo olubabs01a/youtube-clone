5ae3e9ee76ed1999b291f071b8f9ae7837c0a1b7
Funmi B <osb1271.rit@gmail.com>
2024-01-06 22:31:47 -0500

    add firebase functions to creaete users

--- youtube-api-service/functions/src/index.ts
+++ youtube-api-service/functions/src/index.ts
@@ -1,19 +1,18 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as firebase from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+firebase.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const firestore = new firebase.firestore.Firestore();
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  functions.logger.info(`User created: ${JSON.stringify(userInfo)}`);
+  return;
 });
