5e1fdbc31f4005f80d9b875e282aecb40ce250e9
Funmi B <osb1271.rit@gmail.com>
2024-01-07 00:32:07 -0500

    add signed url gen function

--- youtube-api-service/functions/src/index.ts
+++ youtube-api-service/functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from "firebase-functions";
 import * as firebase from "firebase-admin";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 firebase.initializeApp();
 
 const firestore = new firebase.firestore.Firestore();
+const storage = new Storage();
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -15,4 +18,31 @@ export const createUser = functions.auth.user().onCreate((user) => {
   firestore.collection("users").doc(user.uid).set(userInfo);
   functions.logger.info(`User created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if user is authenticated.
+  if (request.auth === undefined) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated.",
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket("en1-yt-raw-vid3os");
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const expires = Date.now() + 15 * 60 * 1000; // 15 minutes
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires,
+  });
+
+  return {url, fileName, expires};
 });
