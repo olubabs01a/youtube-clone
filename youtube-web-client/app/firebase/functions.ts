61c536c9354df3be0d34044d18dc10e0490b9824
Funmi B <osb1271.rit@gmail.com>
2024-01-08 02:48:04 -0500

    formatting

--- youtube-web-client/app/firebase/functions.ts
+++ youtube-web-client/app/firebase/functions.ts
@@ -5,18 +5,18 @@ const functions = getFunctions();
 const generateUploadUrl = httpsCallable(functions, "generateUploadUrl");
 
 export async function uploadVideo(file: File) {
-    const response: any = await generateUploadUrl({
-        fileExtension: file.name.split('.').pop()
-    });
+  const response: any = await generateUploadUrl({
+    fileExtension: file.name.split('.').pop()
+  });
 
-    // Upload the file via signed URL
-    await fetch(response?.data?.url, {
-        method: "PUT",
-        body: file,
-        headers: {
-            "Content-Type": file.type
-        }
-    });
+  // Upload the file via signed URL
+  await fetch(response?.data?.url, {
+    method: "PUT",
+    body: file,
+    headers: {
+      "Content-Type": file.type
+    }
+  });
 
-    return;
+  return;
 }
